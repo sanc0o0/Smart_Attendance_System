97b78f3e477a82098f3635e700a6cbd968120a6d
sanc0o0 <sanansari0305@gmail.com>
2025-12-31 22:01:28 +0530

    Add Morning and Afternoon Sessions timeline

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from datetime import datetime
+from datetime import datetime, time
 import pytz
 
 from .analytics import router as analytics_router
-from . import models
+# from . import models
 from .database import SessionLocal, Base, engine
 from .models import Student, Attendance
 from .schemas import AttendanceRequest
@@ -17,7 +17,19 @@ Base.metadata.create_all(bind=engine)
 IST = pytz.timezone("Asia/Kolkata")
 
 def get_session(time_obj):
-    return "morning" if time_obj.hour < 12 else "afternoon"
+    morning_start = time(8, 0)
+    morning_end = time(12, 59)
+
+    afternoon_start = time(13, 0)
+    afternoon_end = time(17, 30)
+
+    if morning_start <= time_obj <= morning_end:
+        return "morning"
+    elif afternoon_start <= time_obj <= afternoon_end:
+        return "afternoon"
+    else:
+        return None
+
 
 # Dependency
 def get_db():
@@ -44,23 +56,28 @@ def mark_attendance(payload: AttendanceRequest, db: Session = Depends(get_db)):
     today = now_ist.date()
     time_now = now_ist.time()
 
+    session = get_session(time_now)
+    if not session:
+        return {
+            "status": "rejected",
+            "message": "Attendance allowed only in morning or afternoon sessions"
+        }
+
     # 1️⃣ Find or create student
     student = db.query(Student).filter(Student.face_id == face_id).first()
-
     if not student:
         student = Student(name=name, face_id=face_id)
         db.add(student)
         db.commit()
         db.refresh(student)
 
-    # 2️⃣ Insert attendance
-    attendance = Attendance(
-        student_id=student.id,
-        attendance_date=today,
-        attendance_time=time_now
-    )
+    # # 2️⃣ Insert attendance
+    # attendance = Attendance(
+    #     student_id=student.id,
+    #     attendance_date=today,
+    #     attendance_time=time_now
+    # )
 
-    session = get_session(time_now)
 
     existing = db.query(Attendance).filter(
         Attendance.student_id == student.id,
